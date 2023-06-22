a594c5b6a3d058ff507d5fc9473af6c0297b9694
Jawale Kajal (NCS) <jawale.kajal@in.ncs-i.com>
2023-06-22 16:42:00 +0530

    commit

---
+++ AppForLocalJenkins/src/main/java/com/local/AppForLocalJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.local;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppForLocalJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppForLocalJenkinsApplication.class, args);
+	}
+
+}
---
+++ AppForLocalJenkins/src/main/java/com/local/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.local.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+public class Controller {
+
+    @GetMapping("/hello")
+    public String getWorld(){
+        return "Hello jenkins";
+    }
+}
